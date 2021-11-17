bb7e44a90a1a84afefdc50b6b2ad742961b34b06
rafaelpavao <93341589+rafaelpavao@users.noreply.github.com>
2021-11-17 17:59:45 -0300

    First Commit

---
+++ ex_1.py
@@ -0,0 +1,3 @@
+num = int(input('Digite um numero inteiro: '))
+print('Sucessor: ' + str(num + 1))
+print('Antecessor: ' + str(num - 1))
---
+++ ex_2.py
@@ -0,0 +1,7 @@
+import math
+
+
+num = float(input('Digite um numero: '))
+print('Dobro: ' + str(num + num))
+print('Cubo: ' + str(num ** 3))
+print('Raiz Quadrada: ' + str(math.sqrt(num)))
